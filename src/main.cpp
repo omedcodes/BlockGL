445b3b372b348b6ac7f6ffe86a29a6cc22fdf5dd
omedcodes <omedsafi268@gmail.com>
2026-02-01 15:13:26 +0100

    chore: add functionality and scene saving to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "Application/Application.h"
 #include "Performance/Trace.h"
-#include "glCraft.h"
+#include "glCraft.h
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
   START_TRACE("startup.json");
   auto *app = new Application;
   END_TRACE();
 
   START_TRACE("scene-creation.json");
-  std::string savePath = argc > 1 ? argv[1] : "default.glc";
+  std::string savePath = argc > 1 ? argv[1] : "default.glb";
   app->setScene(std::make_shared<Scene>(savePath));
   END_TRACE();
 
