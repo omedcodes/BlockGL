f46c08e9c223a84ec1c3425841e56e9fe23cb750
omedcodes <omedsafi268@gmail.com>
2026-02-01 23:10:10 +0100

    feat: introduce Persistence system

---
+++ src/Persistence/Persistance.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../Scene/Camera.h"
+#include "../Util/Util.h"
+#include "../World/Chunk.h"
+#include "../glBlocks.h"
+
+class Persistence {
+  std::string path;
+  Camera camera;
+  std::unordered_map<glm::ivec2, Ref<Chunk>, Util::HashVec2> chunks;
+
+public:
+  explicit Persistence(std::string path);
+  ~Persistence();
+
+  void commitChunk(const Ref<Chunk>& chunk);
+  [[nodiscard]] Ref<Chunk> getChunk(glm::ivec2 position) const;
+
+  void commitCamera(const Camera& newCamera);
+  [[nodiscard]] const Camera& getCamera() const;
+};
