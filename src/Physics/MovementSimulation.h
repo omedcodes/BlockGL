2bc179181349979a0ed4954999498dda190ebad9
omedcodes <omedsafi268@gmail.com>
2026-02-01 23:16:47 +0100

    feat: add movement simulation collision interface

---
+++ src/Physics/MovementSimulation.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../World/World.h"
+#include "../glBlocks.h"
+
+namespace MovementSimulation {
+    bool canMove(const glm::vec3 &from, const glm::vec3 &to, World &world);
+};
