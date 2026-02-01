4013904f5c913bdd527e01919b908c4725909adf
omedcodes <omedsafi268@gmail.com>
2026-02-01 22:36:18 +0100

    feat: add world ray casting utility

---
+++ src/Math/WorldRayCast.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../World/World.h"
+#include "../glBlocks.h"
+
+struct HitTarget 
+{
+    glm::vec3 position;
+    BlockData block;
+    glm::vec3 neighbor;
+    bool hasNeighbor = false;
+};
+
+class WorldRayCast {
+    bool successful;
+    HitTarget hitTarget;
+
+public:
+    WorldRayCast(glm::vec3 position, glm::vec3 direction, World& world, float reach);
+    
+    [[nodiscard]] bool hasHit() const { return successful; };
+    [[nodiscard]] HitTarget getHitTarget() const { return hitTarget; };
+    explicit operator bool() const { return hasHit(); }
+};
