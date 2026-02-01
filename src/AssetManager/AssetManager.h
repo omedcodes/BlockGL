4fff6f662f6927c81d268181bbeb24d2fe08c2b0
omedcodes <omedsafi268@gmail.com>
2026-02-01 16:39:00 +0100

    feat: add AssetManager load and remove functions

--- src/AssetManager/AssetManager.h
+++ src/AssetManager/AssetManager.h
@@ -22,4 +22,36 @@ class AssetManager {
   CubeMapRegistry cubeMapRegistry;
   ShaderRegistry shaderRegistry;
   ShaderProgramRegistry shaderProgramRegistry;
+
+public:
+  AssetManager();
+  ~AssetManager();
+
+  static AssetManager &instance() { return *instancePtr; };
+
+  void removeTextFromRegistry(const std::string &name) { textRegistry.remove(name); }
+  void removeImageFromRegistry(const std::string &name) { imageRegistry.remove(name); }
+  void removeTextureFromRegistry(const std::string &name) { textureRegistry.remove(name); }
+  void removeCubeMapFromRegistry(const std::string &name) { cubeMapRegistry.remove(name); };
+  void removeShaderFromRegistry(const std::string &name) { shaderRegistry.remove(name); }
+  void removeShaderProgramFromRegistry(const std::string &name) { shaderProgramRegistry.remove(name); }
+
+  Ref<const std::string> loadText(const std::string &name) { return textRegistry.get(name); };
+  Ref<const Image> loadImage(const std::string &name) { return imageRegistry.get(name); };
+  Ref<const Texture> loadTexture(const std::string &name) { return textureRegistry.get(name); };
+
+
+  // the expected input format: width;height;
+  Ref<const Texture> loadTextureArray(const std::string &name) { return textureArrayRegistry.get(name); };
+
+  // the expected input format: right;left;top;bottom;front;back
+  Ref<const Texture> loadCubeMap(const std::string &name) { return cubeMapRegistry.get(name); };
+  Ref<const Shader> loadShader(const std::string &name) { return shaderRegistry.get(name); };
+  Ref<const ShaderProgram> loadShaderProgram(const std::string &name) { return shaderProgramRegistry.get(name); };
+
+  AssetManager(const AssetManager &) = delete;
+  AssetManager(AssetManager &) = delete;
+  AssetManager(AssetManager &&) noexcept = delete;
+  AssetManager &operator=(AssetManager &) = delete;
+  AssetManager &operator=(AssetManager &&) noexcept = delete;
 };
