b5d872323eca3ecd58681ec23636d1194b21d575
omedcodes <omedsafi268@gmail.com>
2026-02-02 11:08:05 +0100

    feat: support image slicing for texture array generation

---
+++ src/Rendering/Image.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../glBlocks.h"
+
+struct Image {
+  uint32_t width, height;
+  std::vector<uint8_t> data;
+
+  Image subImage(glm::uvec2 offset, glm::uvec2 extent) const;
+};
