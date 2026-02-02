d52863826086fe0271fa1fb42c99cb666dffc9b2
omedcodes <omedsafi268@gmail.com>
2026-02-02 11:13:17 +0100

    feat: add Framebuffer abstraction with color and depth attachments

---
+++ src/Rendering/FramebufferStack.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../glBlocks.h"
+#include "FrameBuffer.h"
+
+class FramebufferStack {
+  std::vector<Ref<Framebuffer>> stack;
+  std::vector<Ref<Texture>> intermediateTextures;
+  bool keepIntermediateTextures = false;
+
+public:
+  void push(const Ref<Framebuffer>& framebuffer);
+  [[nodiscard]] Ref<Framebuffer> peek() const;
+  Ref<Framebuffer> pop();
+
+  void setKeepIntermediateTextures(bool keepBuffers);
+  void clearIntermediateTextureReferences();
+  std::vector<Ref<Texture>> getIntermediateTextures() const;
+
+  [[nodiscard]] bool empty() const;
+  [[nodiscard]] size_t size() const;
+};
